db344f4b95c4611a723862e96f08d8ef302e44b1
Ramiro Lamilla <raugusto.lamilla@gmail.com>
2023-10-22 20:41:11 -0500

    first commit

--- app/src/main/java/com/example/reto1/MainActivity.java
+++ app/src/main/java/com/example/reto1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.reto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void gotoRegistro(View view){
+        Intent intent=new Intent(MainActivity.this,Registro.class);
+        startActivity(intent);
     }
 }
